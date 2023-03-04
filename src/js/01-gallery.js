faf123c2349d39f968b824418f254eeab2e65dfb
jane05070 <jane05070@gmail.com>
2023-03-04 21:56:33 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,50 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const divRef = document.querySelector('.gallery');
+
+function createGallaryMarkup(items) {
+    return items.map((item) =>
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+            </a>
+        </div>`
+
+    ).join("");
+}
+
+const addGallaryMurkup = createGallaryMarkup(galleryItems);
+
+divRef.innerHTML = addGallaryMurkup;
+
+divRef.addEventListener("click", handleImageClick);
+
+function handleImageClick(event) {
+    blockStandartAction(event);
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src = "${event.target.dataset.source}" width = "800" height = "600">`);
+    instance.show();
+
+    divRef.addEventListener("keydovn", (event) => {
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    })
+}
+
+function blockStandartAction(event) {
+    event.preventDefault();
+}
